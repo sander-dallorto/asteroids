768072243f2c1bdf666e07eac9be9df2a9945f67
sander-dallorto <sanderrodriguesr75@gmail.com>
2025-01-29 19:50:23 -0300

    features added

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import Player
 
 def main():
     running = True
     
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,8 +22,10 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-
+        ms = clock.tick(60)
+        dt = ms/1000
 
 if __name__ == "__main__":
     main()
